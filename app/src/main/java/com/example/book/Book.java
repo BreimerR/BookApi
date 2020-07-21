76da068b0082aa4690b9bf6e1f8118aaaa0559f8
Isabel <lusabe2010@gmail.com>
2020-07-21 23:06:36 +0300

    Initial commit

---
+++ app/src/main/java/com/example/book/Book.java
@@ -0,0 +1,12 @@
+package com.example.book;
+
+public class Book {
+    public String id;
+    public String title;
+    public String subtitle;
+    public String[] authors;
+    public String publisher;
+    public String publisherDate;
+
+
+    }
